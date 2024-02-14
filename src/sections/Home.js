3254a6ea00fc8bdf47ef9ef389faf815f1bf5ded
Semaj Casteel <thepronicole888@gmail.com>
2024-02-14 15:02:33 -0600

    created home w scroll

---
+++ src/sections/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.section`
+  position: relative;
+  min-height: 200vh;
+  overflow: hidden;
+`
+
+const Home = () => {
+  return (
+    <Section>Home</Section>
+  )
+}
+
+export default Home
